6076da4e50d8e19493190a611858151ca18fd63b
willy <willy_guo@gmail.com>
2023-01-13 16:23:45 +0800

    加入專案檔案。

---
+++ PMRDC/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace PMRDC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbltimer = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Khaki;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PMRDC程式登入平台";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(116, 89);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.groupBox1.Size = new System.Drawing.Size(417, 185);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thermal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(94, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "6SIGMA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(258, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FLOTHERM XT\r\n(尚未開發)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(66, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(536, 60);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "* 依軟體使用辦法，請勿長時間占用\r\n* 若有急需使用軟體，但無License情況請聯絡RDPM(Marcus Kuo #33930)\r\n* 平台使用問題請聯絡W" +
+    "illy Guo(#32725)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(462, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "使用者 :";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button3.Location = new System.Drawing.Point(282, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(107, 48);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::PMRDC.Properties.Resources._6sigma;
+            this.button1.Location = new System.Drawing.Point(59, 39);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 80);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::PMRDC.Properties.Resources.FloTHERM_XT;
+            this.button2.Location = new System.Drawing.Point(234, 39);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 80);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lbltimer
+            // 
+            this.lbltimer.AutoSize = true;
+            this.lbltimer.Location = new System.Drawing.Point(31, 368);
+            this.lbltimer.Name = "lbltimer";
+            this.lbltimer.Size = new System.Drawing.Size(33, 12);
+            this.lbltimer.TabIndex = 8;
+            this.lbltimer.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(631, 445);
+            this.Controls.Add(this.lbltimer);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label lbltimer;
+    }
+}
+
---
+++ PMRDC/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Principal;
+using System.Reflection.Emit;
+
+namespace PMRDC
+{
+    public partial class Form1 : Form
+    {
+        int timerCount = 0;
+        bool timerStart = false;
+        int Pastx;
+        int Nowx = int.Parse(string.Format("{0}", System.Windows.Forms.Cursor.Position.X));
+        int xrepeat = 0;
+        bool Sigma_exist;
+        string strUserName = WindowsIdentity.GetCurrent().Name;
+
+        public bool Simga_existFuc()
+        {
+            Process[] processes = Process.GetProcessesByName("6SigmaET");
+            if (processes.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool ClosePress(string FileName)//關閉外部檔案
+        {
+            Process[] MyProcess = Process.GetProcessesByName(FileName);
+            if (MyProcess.Length > 0)
+                MyProcess[0].Kill(); //關閉執行中的程式
+            return true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] aryUserInfo = strUserName.Split(new string[] { "\\" }, StringSplitOptions.RemoveEmptyEntries);
+            label5.Text = "使用者 : " + aryUserInfo[1];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //顯示滑鼠移動座標
+            lbltimer.Text = string.Format("時間：" + DateTime.Now.ToString("HH:mm:ss") + "，X：{0}，Y：{1}"
+            , System.Windows.Forms.Cursor.Position.X, System.Windows.Forms.Cursor.Position.Y);
+            if (timerCount == 0)
+            {
+                Nowx = int.Parse(string.Format("{0}", System.Windows.Forms.Cursor.Position.X));
+                ++timerCount;
+            }
+            else
+            {
+                Pastx = Nowx;
+                Nowx = int.Parse(string.Format("{0}", System.Windows.Forms.Cursor.Position.X));
+                Sigma_exist = Simga_existFuc();
+                if (Sigma_exist)
+                {
+                    if (Pastx == Nowx)
+                    {
+                        ++xrepeat;
+                    }
+                    else
+                    {
+                        xrepeat = 0;
+                    }
+                }
+                else
+                {
+                    this.Dispose();
+                }
+
+            }
+            if (xrepeat == 8)
+            {
+                string text = "電腦已閒置45分鐘，請立即存檔。系統若閒置60分鐘，將強制關閉6Sigma。";
+                MessageBox.Show(text);
+            }
+            if (xrepeat == 11)
+            {
+                string text = "關閉程式";
+                ClosePress("6SigmaET");//關閉外部檔案
+                //this.Dispose();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //先判斷程式有沒有正確開啟
+            //讓程式在工具列中隱藏
+            this.ShowInTaskbar = false;
+            //隱藏程式本身的視窗
+            this.Hide();
+            this.notifyIcon1.Visible = true;
+            Sigma_exist = Simga_existFuc();
+            if (Sigma_exist)
+            {
+                if (timerStart == false)
+                {
+                    timerStart = true;
+                    timer1.Interval = 200;
+                    timer1.Tick += new EventHandler(timer1_Tick);
+                    timer1.Enabled = true;
+                }
+            }
+            else
+            {
+                if (timerStart == false)
+                {
+                    Process Sigma6 = new Process();
+                    // FileName 是要執行的檔案
+                    Sigma6.StartInfo.FileName = "C:\\Program Files\\6SigmaDCRelease15\\6SigmaET.exe";
+                    Sigma6.Start();
+                    timerStart = true;
+                    bool SigmaOpen = true;
+                    timer1.Interval = 1000;
+                    timer1.Tick += new EventHandler(timer1_Tick);
+                    timer1.Enabled = true;
+                }
+
+            }
+        }
+    }
+}
