8fe4ac51ca816eafdb179ca008929b0054b4eeba
willy <willy_guo@gmail.com>
2023-05-10 11:26:35 +0800

    0510

--- PMRDC/Form1.cs
+++ PMRDC/Form1.cs
@@ -39,7 +39,7 @@ namespace PMRDC
         //取得目前登入的帳號
         string strUserName = WindowsIdentity.GetCurrent().Name;
         //此系統版本
-        string Version = "v20230505";
+        string Version = "v20230510";
         //紀錄6sigma開啟時間
         DateTime timeminstr;
         //紀錄6sigma關閉時間
